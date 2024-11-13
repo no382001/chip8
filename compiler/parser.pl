1a723f6f49b043d121a9e01d03283da11d8acd67
no382001 <102482527+no382001@users.noreply.github.com>
2024-11-13 18:12:21 +0100

    some more syntax

--- compiler/parser.pl
+++ compiler/parser.pl
@@ -25,21 +25,24 @@ declaration_list([assign(Var, Expr)]) -->
 statement(assign(Var, Expr)) --> variable(Var), ws, "=", ws, expression(Expr), ";", ws.
 statement(declaration(Var, Value)) --> "auto", ws, variable(Var), ws, "=", ws, expression(Value), ";", ws.
 statement(declaration_list(Declarations)) --> "auto", ws, declaration_list(Declarations), ";", ws.
-statement(while(Cond, Body)) --> "while(", ws, expression(Cond), ")", ws, block(Body).
+statement(while(Cond, Body)) --> "while", ws, "(", ws, expression(Cond), ")", ws, block(Body).
 statement(if_else(Cond, IfBody, ElseBody)) --> if_statement(Cond, IfBody), branching(ElseBody).
 statement(fundecl(Func, Args, Body)) --> function_call(Func, Args), block(Body), ws.
 statement(funcall(Func, Args)) --> function_call(Func, Args), ";", ws.
 
 if_statement(Cond, IfBody) -->
-    "if(", ws, expression(Cond), ")", ws, block(IfBody), ws.
+    "if", ws , "(", ws, expression(Cond), ")", ws, block(IfBody), ws.
 
 branching(ElseBody) -->
     "else", ws, block(ElseBody).
+branching(if_else(Cond,IfBody,ElseBody)) -->
+    "else", ws, if_statement(Cond,IfBody), ws, branching(ElseBody).
 branching([]) --> [].
 
 block(Body) --> "{", ws, statements(Body), "}", ws.
 
 expression(num(N)) --> integer(N).
+expression(num(N)) --> hex_number(N).
 expression(var(Var)) --> variable(Var).
 expression(binop(Op, Lhs, Rhs)) --> term(Lhs), ws, operator(Op), ws, expression(Rhs).
 expression(X) --> enumeration(X).
@@ -114,29 +117,55 @@ repl :-
     ).
 
 main :-
-    run_test("test 1: variable assignment", "r = 5;", [assign(r, num(5))]),
+    run_test("test 1: variable assignment",
+        "r = 5; r = 0xff;",
+        [assign(r, num(5)), assign(r, num(255))]),
     
-    run_test("test 2: variable declaration", "auto x = 10;", [declaration(x, num(10))]),
+    run_test("test 2: variable declaration",
+        "auto x = 10;",
+        [declaration(x, num(10))]),
     
-    run_test("test 3: while loop", "while(x < 10) { x = x + 1; }",
-             [while(binop(<, var(x), num(10)), [assign(x, binop(+, var(x), num(1)))] )]),
+    run_test("test 3: while loop",
+        "while(x < 10) { x = x + 1; }",
+        [while(binop(<, var(x), num(10)), [assign(x, binop(+, var(x), num(1)))] )]),
     
-    run_test("test 4: if-else statement", "if(x == 5) { y = 1; } else { y = 0; }",
-             [if_else(binop(==, var(x), num(5)), [assign(y, num(1))], [assign(y, num(0))])]),
-    
-    run_test("test 5: function call", "draw(x, y);", [funcall(draw, [var(x), var(y)])]),
-    
-    run_test("test 6: block with multiple statements", "auto x = 0; auto y = 1;",
-             [declaration(x, num(0)), declaration(y, num(1))]),
-    
-    run_test("test 7: complex assignment", "result = x + y * z;", 
-             [assign(result, binop(+, var(x), binop(*, var(y), var(z))))]),
-    
-    run_test("test 8: array assignment", "r = { 0x80, 0x40, 0x20, 0x10 };",
-             [assign(r, [num(128), num(64), num(32), num(16)])]),
+    run_test("test 4.0: if statement",
+        "if (x == 5) { y = 1; }",
+        [if_else(binop(==, var(x), num(5)), [assign(y, num(1))],[])]),
 
-    run_test("test 9: single line comment", "r = { 0x80, 0x40, 0x20, 0x10 }; // this is a comment",
-             [assign(r, [num(128), num(64), num(32), num(16)])]).
+    run_test("test 4.1: if-else statement",
+        "if (x == 5) { y = 1; } else { y = 0; }",
+        [if_else(binop(==, var(x), num(5)), [assign(y, num(1))], [assign(y, num(0))])]),
+    
+    run_test("test 4.2: if-else-if statement",
+        "if (x == 5) { y = 1; } else if (x == 4) { y = 0; }",
+        [if_else(
+            binop(==,var(x),num(5)),
+            [assign(y,num(1))],
+            if_else(
+                binop(==,var(x),num(4)),
+                [assign(y,num(0))],[]))]),
+        
+
+    run_test("test 5: function call",
+        "draw(x, y);",
+        [funcall(draw, [var(x), var(y)])]),
+    
+    run_test("test 6: block with multiple statements",
+        "auto x = 0; auto y = 1;",
+        [declaration(x, num(0)), declaration(y, num(1))]),
+    
+    run_test("test 7: complex assignment",
+        "result = x + y * z;", 
+        [assign(result, binop(+, var(x), binop(*, var(y), var(z))))]),
+    
+    run_test("test 8: array assignment",
+        "r = { 0x80, 0x40, 0x20, 0x10 };",
+        [assign(r, [num(128), num(64), num(32), num(16)])]),
+
+    run_test("test 9: single line comment",
+        "r = { 0x80, 0x40, 0x20, 0x10 }; // this is a comment",
+        [assign(r, [num(128), num(64), num(32), num(16)])]).
 
 
 parse_file(FileName, AST) :-
