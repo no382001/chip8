a334e0f46df1ab12922a75a2b793eaba128a2e9a
no382001 <102482527+no382001@users.noreply.github.com>
2024-12-15 21:05:32 +0100

    error semantics 2

--- compiler/parser_err.pl
+++ compiler/parser_err.pl
@@ -24,6 +24,7 @@ variable(Var) -->
     [C], { char_type(C, alpha) },
     variable_chars(Cs),
     { atom_codes(Var, [C|Cs]) }.
+variable('') --> [].
 
 variable_chars([C|Cs]) --> [C], { char_type(C, alnum) }, variable_chars(Cs).
 variable_chars([]) --> [].
@@ -37,7 +38,7 @@ statements(Statements) -->
 
 % Bs represents the semantics defined up to this point -- in reversed order,
 % Stms is the actual semantics
-statements(Bs,Stms) --> statement(Bs,X), !, ws, statements([X|Bs],Stms).
+statements(Bs,Stms) --> ws, statement(Bs,X), !, ws, statements([X|Bs],Stms).
 statements(Bs,Stms) -->
     nonempty(Tokens), !, { string_codes(Ts, Tokens), Stms = [error('syntax error',Ts)|Bs] }.
 statements(Bs,Bs) --> ws.
@@ -49,7 +50,7 @@ is_declared(Bs,Var) :-
 
 statement(Bs,Stms) -->
     % simple assignment
-    variable(Var), ws, "=", ws, expression(Bs,Expr), ws, ";", ws, !,
+    variable(Var), { Var \= '' }, ws, "=", ws, expression(Bs,Expr), ws, ";", ws, !,
     {
         is_declared(Bs,Var)
             ->  Stms = assign(Var, Expr)
@@ -58,7 +59,7 @@ statement(Bs,Stms) -->
 
 statement(Bs,Stms) -->
     % simple declaration
-    "byte", ws, variable(Var), ws, ";", !,
+    "byte", ws, variable(Var), { Var \= '' }, ws, ";", !,
     {
         is_declared(Bs,Var)
         ->  Stms = error('redeclaration of variable',Var)
@@ -67,14 +68,15 @@ statement(Bs,Stms) -->
 
 statement(Bs,Stms) -->
     % declaration with value
-    "byte", ws, variable(Var), ws, "=", ws, expression(Bs,Expr), ws, ";", ws, !,
+    "byte", ws, variable(Var), { Var \= '' }, ws, "=", ws, expression(Bs,Expr), ws, ";", ws, !,
     {
         is_declared(Bs,Var)
         ->  Stms = error('redeclaration of variable',[Var,=,Expr])
-        ;   Expr = error(_,_)
-            -> Stms = Expr % pass the error
-            ;   Stms = declassign(Var, Expr) % flatten this later
+        ;   Stms = declassign(Var, Expr)
     }.
+
+statement(_,Stms) -->
+    nonempty(Tokens), !, { string_codes(Ts, Tokens), Stms = error('invalid statement',Ts) }.
 
 % ----------------------------
 % expression
@@ -90,17 +92,14 @@ expression(Bs,Stm) --> variable(Var),
     {
         is_declared(Bs,Var)
         -> Stm = var(Var)
-        ; Stm = error('use while-in/before declaration',var(Var))
+        ;   Var = ''
+            ->  fail  
+            ;   Stm = error('use while-in/before declaration',var(Var))
     }.
 expression(Bs,Result) -->
-    expression(Bs,Lhs), ws, operator(Op), ws, expression(Bs,Rhs),
+    expression(Bs,Lhs), !, ws, operator(Op), ws, expression(Bs,Rhs),
     {
-        (   Lhs = error(_, _)
-        ->  Result = Lhs
-        ;   Rhs = error(_, _)
-        ->  Result = Rhs
-        ;   Result = binop(Op, Lhs, Rhs)
-        )
+        Result = binop(Op, Lhs, Rhs)
     }.
 
 operator(+) --> "+".
@@ -111,40 +110,53 @@ operator(mod) --> "mod".
 operator(<) --> "<".
 operator(>) --> ">".
 operator(==) --> "==".
-operator(invalid) --> { throw(error('invalid_operator','invalid_operator')) }.
+operator(error('invalid operator',C)) --> [C].
 
 parse(S,R) :-
     string_codes(S, SC),
     statements(R,SC,[]).
 
 run_test(Input, Expected) :-
-    string_codes(Input, InputCodes),
-    ( phrase(statements(Ast), InputCodes),
-        Ast = Expected -> 
-        format("~w: passed~n", [Input])
-    ; 
-        format("~w: failed~nexpected: ~w~ngot: ~w~n", [Input, Ast])
+    parse(Input,R),
+    (   R = Expected
+        ->  format("~w: passed~n", [Input])
+        ;   format("~w: failed~nexpected: ~w~ngot: ~w~n", [Input, Expected, R])
     ).
 
 tests :-
+    run_test(";", [error('invalid statement',";")]),
+    run_test("", []),
     run_test("v = 1;",
         [error('assignment before declaration', [v, =, num(1)])]),
     run_test("byte v;",
         [declare(v)]),
     run_test("byte",
-        [error('syntax error', "byte")]),
+        [error('invalid statement', "byte")]),
     run_test("byte v = 1;",
         [declassign(v, num(1))]),
+    run_test(" byte   x   =  5   ; ",
+        [declassign(x, num(5))]),        
     run_test("byte v = 255;",
         [declassign(v, num(255))]),
     run_test("byte v = 123123;",
-        [error('byte representation failed', 123123)]),
+        [declassign(v,error('byte representation failed',123123))]),
     run_test("byte v = 1 + 1 + 1 + 1;",
-        [declassign(v, binop(+, num(1), binop(+, num(1), binop(+, num(1), num(1)))))]).
-
+        [declassign(v, binop(+, num(1), binop(+, num(1), binop(+, num(1), num(1)))))]),
+    run_test("byte v = 1 + 1 + 1 + v;",
+        [declassign(v, binop(+, num(1), binop(+, num(1), binop(+, num(1), error('use while-in/before declaration', var(v))))))]),
+    run_test("byte x; byte x = 5;",
+        [declare(x), error('redeclaration of variable', [x, =, num(5)])]),
+    run_test("x = 1 + y;",
+        [error('assignment before declaration', [x, =, binop(+, num(1), error('use while-in/before declaration', var(y)))])]),
+    run_test("byte x = 10; x = x + 5;",
+        [declassign(x, num(10)), assign(x, binop(+, var(x), num(5)))]),
+    run_test("byte;", [error('invalid statement',"byte;")]),
+    run_test("x =", [error('invalid statement',"x =")]),
+    run_test("byte x = ;", [error('syntax error', "byte x = ;")]).
+    
+        
+        
 % parse("byte v = 1 + 1 + 1 + v;",T).
-
-% throw and catch things?
 
 %!!.	Repeat last query
 %!nr.	Repeat query numbered <nr>
@@ -153,3 +165,10 @@ tests :-
 %!h.	Show this list
 
 % parse("byte v = 0x1;",T).
+
+% List = [+, 1, 1],
+% Term =.. List,
+% R is Term.
+
+% values should wrap around 256
+% should i throw, and catch(phrase(..)..) in the dcg?
